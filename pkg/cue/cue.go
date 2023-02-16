6fdaf615aadff5a6868ae3f7eb47c33f3813e8d4
Marcel Heers <info@heers.it>
2023-02-16 16:48:30 +0200

    exports conversion helpers

--- pkg/cue/cue.go
+++ pkg/cue/cue.go
@@ -9,40 +9,65 @@ import (
 )
 
 func Exists(data, path string) bool {
-	v := cueValue(data)
+	v, err := StringToCueValue(data)
+	if err != nil {
+		return false
+	}
 	return value.Exists(v, path)
 }
 
 func Set(data, path string, x interface{}) (string, error) {
-	v := cueValue(data)
+	v, err := StringToCueValue(data)
+	if err != nil {
+		return "", err
+	}
 	result, err := value.Set(v, path, x)
 	if err != nil {
 		return "", err
 	}
 
-	return cueString(result), nil
+	return CueValueToString(result), nil
 }
 
 func Get(data, path string, result interface{}) error {
-	v := cueValue(data)
+	v, err := StringToCueValue(data)
+	if err != nil {
+		return err
+	}
 	return value.Get(v, path, result)
 }
 
 func Replace(data, path string, x interface{}) (string, error) {
-	v := cueValue(data)
+	v, err := StringToCueValue(data)
+	if err != nil {
+		return "", err
+	}
 	result, err := value.Replace(v, path, x)
 	if err != nil {
 		return "", err
 	}
 
-	return cueString(result), nil
+	return CueValueToString(result), nil
 }
 
-func cueString(v cue.Value) string {
+func Format(data string) (string, error) {
+	v, err := StringToCueValue(data)
+	if err != nil {
+		return "", err
+	}
+	return CueValueToString(v), nil
+}
+
+func CueValueToString(v cue.Value) string {
 	return fmt.Sprintf("%.2v", v)
 }
 
-func cueValue(data string) cue.Value {
+func StringToCueValue(data string) (cue.Value, error) {
 	ctx := cuecontext.New()
-	return ctx.CompileString(data)
+	v := ctx.CompileString(data)
+	if err := v.Err(); err != nil {
+		return cue.Value{}, err
+	}
+
+	return v, nil
 }
